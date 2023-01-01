f234a376cd6a3c60c074ddfc6532503fa144bc05
Athira S <61055595+aathira2001@users.noreply.github.com>
2023-01-01 14:17:02 +0530

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,6 @@
+import calendar
+import datetime
+x = datetime.datetime.now()
+print("Today's date and Time is:",x)
+print("Happy New Year....,The calendar of year 2023 is here:")
+print(calendar.calendar(2023,2,2,6))
